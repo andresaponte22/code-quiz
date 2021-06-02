bceccecccc49c1f24f4a94b8976c10d4a3581d61
Andres Aponte <andresaponte.f@gmail.com>
2021-06-02 16:17:01 -0400

    Added highscore functionality and display

--- assets/script.js
+++ assets/script.js
@@ -1,111 +1,157 @@
 // Variables
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("answers");
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-btns')
+var highscoreContainerElement = document.getElementById('highscore-container')
+var usernameInput = document.getElementById('username')
+var submitButtonElement = document.getElementById('submitUsername')
 
-const quizQuestions = [
+var highscoreDisplayElement = document.getElementById('highscore-display')
+
+var usernameDisplay = document.getElementById('username-input')
+var scoreDisplay = document.getElementById('score')
+
+let shuffledQuestions, currentQuestionIndex
+var countRightAnswers = 0
+let timer = 0
+
+// Object and keys for quiz questions and answers
+const questions = [
   {
     question: "Commonly used data types DO NOT include:",
-    answers: {
-      a: "Booleans",
-      b: "Strings",
-      c: "Alerts",
-      d: "Numbers"
-    },
-    correctAnswer: "c"
+    answers: [
+      {text: 'Booleans', correct: false},
+      {text: 'Strings', correct: false},
+      {text: 'Alerts', correct: true},
+      {text: 'Numbers', correct: false}
+    ]
   },
   {
     question: "The conditions in an if/else statement are enclosed within:",
-    answers: {
-      a: "Quotes",
-      b: "Curly brackets",
-      c: "Parentheses",
-      d: "Square brackets"
-    },
-    correctAnswer: "c"
+    answers: [
+      {text: 'Quotes', correct: false},
+      {text: 'Curly brackets', correct: false},
+      {text: 'Parentheses', correct: true},
+      {text: 'Square brackets', correct: false}
+    ]
   },
   {
     question: "Arrays in Javascript can be used to store ______.",
-    answers: {
-      a: "Numbers",
-      b: "Other arrays",
-      c: "Booleans",
-      d: "All of the above"
-    },
-    correctAnswer: "d"
+    answers: [
+      {text: 'Numbers', correct: false},
+      {text: 'Other arrays', correct: false},
+      {text: 'Booleans', correct: false},
+      {text: 'All of the above', correct: true}
+    ]
   },
   {
     question: "String values need to be enclosed within _____ when being assinged to variables",
-    answers: {
-      a: "Commas",
-      b: "Curly brackets",
-      c: "Quotes",
-      d: "Parentheses"
-    },
-    correctAnswer: "c"
+    answers: [
+      {text: 'Commas', correct: false},
+      {text: 'Curly brackets', correct: false},
+      {text: 'Quotes', correct: true},
+      {text: 'Parentheses', correct: false}
+    ]
   }
 ];
 
+// Event listener for click on buttons
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+submitButtonElement.addEventListener('click', function() {
+  console.log(usernameInput.value)
+
+  localStorage.setItem("username", usernameInput.value)
+  localStorage.setItem("score", countRightAnswers)
+
+  questionContainerElement.classList.add('hide')
+  highscoreContainerElement.classList.add('hide')
+
+  highscoreDisplayElement.classList.remove('hide')
+
+  usernameDisplay.textContent = localStorage.getItem("username")
+  scoreDisplay.textContent = localStorage.getItem("score")
+
+  startButton.setAttribute('disabled', false)
+
+
+})
+
+
 // Functions
-function buildQuiz(){
-  const output = [];
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
 
-  quizQuestions.forEach(
-    (currentQuestion, questionNumber) => {
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-      var answers = [];
-
-      for(letter in currentQuestion.answers){
-
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
     }
-  );
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
-  quizContainer.innerHTML = output.join('');
-};
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
 
-function showResults(){
+function selectAnswer(e) {
+  var selectedButton = e.target
+  var correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+    startButton.setAttribute('disabled', true)
+    // add highscore functionality
+    highscoreContainerElement.classList.remove('hide')
+  }
+  if (selectedButton.dataset = correct) {
+    countRightAnswers++
+  }
+  document.getElementById('right-answers').innerHTML = countRightAnswers
+}
 
-  var answerContainers = quizContainer.querySelectorAll('.answers');
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
 
-  var numCorrect = 0;
-
-  quizQuestions.forEach( (currentQuestion, questionNumber) => {
-
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    if(userAnswer === currentQuestion.correctAnswer){
-      numCorrect++;
-      answerContainers[questionNumber].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[questionNumber].style.color = 'red';
-    }
-  });
-
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-};
-
-
-
-
-// Initiate quiz 
-buildQuiz();
-
-// Event listeners for answer submission
-submitButton.addEventListener('click', displayResults);
-
-
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
